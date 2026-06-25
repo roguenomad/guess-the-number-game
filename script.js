06d85d450bffc78c7271edf4babaceca9a4668ee
Nohely Garcia <connectedconfessions@gmail.com>
2026-06-25 15:03:15 -0700

    retrieve input number

--- script.js
+++ script.js
@@ -10,5 +10,6 @@
 // console.log(document.querySelector('.guess').value);  
 document.querySelector('.check').addEventListener
 ('click', function() {
-    console.log(document.querySelector('.guess').value);
+    const guess = Number(document.querySelector('.guess').value);
+    console.log(guess, typeof guess);
 });
